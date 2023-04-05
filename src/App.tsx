6ffebab9491d563bbc815e640c232ed8d18ea342
Ballphantom <nokaraza012@gmail.com>
2023-04-05 17:21:39 +0700

    decorate form

--- src/App.tsx
+++ src/App.tsx
@@ -29,66 +29,75 @@ const validate = async (values: any): Promise<ValidationErrors> => {
 const App = () => {
   return (
     <div className="form-container">
-      <Form onSubmit={onSubmit} validate={validate}>
-        {({ handleSubmit, submitting }) => (
-          <form onSubmit={handleSubmit}>
-            <div className="form-header"><h2>- BAN NONG KOK SCHOOL -</h2></div>
-            <div className="form-field">
-              <label>First Name</label>
-              <Field name="firstName" component="input" type="text" />
-            </div>
-            <div className="form-field">
-              <label>Last Name</label>
-              <Field name="lastName" component="input" type="text" />
-            </div>
-            <div className="form-field">
-              <label>Email</label>
-              <Field name="email" component="input" type="email" />
-            </div>
-            <div className="form-field">
-              <label>Telephone number</label>
-              <Field name="telephoneNumber" component="input" type="text" />
-            </div>
-            <div className="form-field">
-              <label>Gender</label>
-              <Field name="gender" component="select" type="text">
-                <option value="none"></option>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-              </Field>
-            </div>
-            <div className="form-field">
-              <label>GPA</label>
-              <Field name="gpa" component="input" type="number" step="0.01" />
-            </div>
-            <div className="form-field">
-              <label>Birthday</label>
-              <Field name="birthday" component="input" type="date" />
-            </div>
-            
-            <div className="form-field">
-              <label>Age</label>
-              <Field name="age" component="input" type="number" />
-            </div>
+      <div className="form-section">
+        <Form onSubmit={onSubmit} validate={validate}>
+          {({ handleSubmit, submitting }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="form-header">
+                <h2 className="title">
+                  <hr />
+                  <b>- BAN NONG KOK SCHOOL -</b>
+                  <hr />
+                </h2>
+              </div>
+              <div className="form-field">
+                <label>First Name</label>
+                <Field name="firstName" component="input" type="text" placeholder="John"/>
+              </div>
+              <div className="form-field">
+                <label>Last Name</label>
+                <Field name="lastName" component="input" type="text" placeholder="Smith"/>
+              </div>
+              <div className="form-field">
+                <label>Email</label>
+                <Field name="email" component="input" type="email" placeholder="email@gmail.com"/>
+              </div>
+              <div className="form-field">
+                <label>Telephone number</label>
+                <Field name="telephoneNumber" component="input" type="text" placeholder="+66 0945781234" />
+              </div>
+              <div className="form-field">
+                <label>Gender</label>
+                <Field name="gender" component="select" type="text">
+                  <option value="-">None</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </Field>
+              </div>
+              <div className="form-field">
+                <label>GPA</label>
+                <Field name="gpa" component="input" type="number" step="0.01" placeholder="4.00" />
+              </div>
+              <div className="form-field">
+                <label>Birthday</label>
+                <Field name="birthday" component="input" type="date" />
+              </div>
 
-            <div className="form-field">
-              <label>Address</label>
-              <Field name="address" component="input" type="text" />
-            </div>
-            <div className="form-field">
-              <label>ID card</label>
-              <Field name="citizenID" component="input" type="text" />
-            </div>
-            <div className="form-field">
-              <label>Class</label>
-              <Field name="class" component="input" type="text" />
-            </div>
-            <button type="submit" disabled={submitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Form>
+              <div className="form-field">
+                <label>Age</label>
+                <Field name="age" component="input" type="number" placeholder="How old are you?"/>
+              </div>
+              <div className="form-field">
+                <label>Address</label>
+                <Field name="address" component="input" type="text" />
+              </div>
+              <div className="form-field">
+                <label>ID card</label>
+                <Field name="citizenID" component="input" type="text" />
+              </div>
+              <div className="form-field">
+                <label>Class</label>
+                <Field name="class" component="input" type="text" />
+              </div>
+              <div className="submit-button">
+                <button type="submit" disabled={submitting}>
+                  Submit
+                </button>
+              </div>
+            </form>
+          )}
+        </Form>
+      </div>
     </div>
   );
 };
